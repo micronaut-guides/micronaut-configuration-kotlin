e3ae39bb9966295ed796cb407f8bdaa85be4d0aa
Sergio del Amo <sergio.delamo@softamo.com>
2020-06-16 09:13:26 +0200

    update to micronaut 2.0

--- complete/src/test/kotlin/example/micronaut/StadiumConfigurationTest.kt
+++ complete/src/test/kotlin/example/micronaut/StadiumConfigurationTest.kt
@@ -1,34 +1,26 @@
 package example.micronaut
 
-import io.kotlintest.shouldBe
-import io.kotlintest.specs.BehaviorSpec
 import io.micronaut.context.ApplicationContext
 import io.micronaut.inject.qualifiers.Qualifiers
-import io.micronaut.test.annotation.MicronautTest
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
 
-@MicronautTest
-class StadiumConfigurationTest : BehaviorSpec({
-
-    given("an application context started with configuration") {
-        val items = mapOf("stadium.fenway.city" to "Boston", // <1>
-                "stadium.fenway.size" to  60000,
-                "stadium.wrigley.city" to  "Chicago", // <1>
-                "stadium.wrigley.size" to 45000
-        )
+class StadiumConfigurationTest {
+    @Test
+    fun testStadiumConfiguration() {
+        val items: MutableMap<String, Any> = HashMap()
+        items["stadium.fenway.city"] = "Boston" // <1>
+        items["stadium.fenway.size"] = 60000
+        items["stadium.wrigley.city"] = "Chicago" // <1>
+        items["stadium.wrigley.size"] = 45000
         val ctx = ApplicationContext.run(ApplicationContext::class.java, items)
-
-        `when`("StadiumConfiguration are retrieved with bean qualifier") {
-            val fenwayConfiguration = ctx.getBean(StadiumConfiguration::class.java, Qualifiers.byName("fenway"))  // <2>
-            val wrigleyConfiguration = ctx.getBean(StadiumConfiguration::class.java, Qualifiers.byName("wrigley")) // <2>
-
-            then("configuration properties are populated") {
-                fenwayConfiguration.name shouldBe "fenway"
-                fenwayConfiguration.size shouldBe 60000
-                wrigleyConfiguration.name shouldBe "wrigley"
-                wrigleyConfiguration.size shouldBe 45000
-            }
-        }
-
+        // <2>
+        val fenwayConfiguration = ctx.getBean(StadiumConfiguration::class.java, Qualifiers.byName("fenway"))
+        val wrigleyConfiguration = ctx.getBean(StadiumConfiguration::class.java, Qualifiers.byName("wrigley"))
+        Assertions.assertEquals("fenway", fenwayConfiguration.name)
+        Assertions.assertEquals(60000, fenwayConfiguration.size)
+        Assertions.assertEquals("wrigley", wrigleyConfiguration.name)
+        Assertions.assertEquals(45000, wrigleyConfiguration.size)
         ctx.close()
     }
-})
+}
--- complete/src/test/kotlin/example/micronaut/TeamConfigurationTest.kt
+++ complete/src/test/kotlin/example/micronaut/TeamConfigurationTest.kt
@@ -1,28 +1,69 @@
 package example.micronaut
 
-import io.kotlintest.shouldBe
-import io.kotlintest.specs.BehaviorSpec
 import io.micronaut.context.ApplicationContext
-import io.micronaut.test.annotation.MicronautTest
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import java.util.*
 import java.util.function.Consumer
+import kotlin.collections.HashMap
 
-@MicronautTest
-class TeamConfigurationTest : BehaviorSpec({
-    given("an application context started with configuration") {
-        val names = listOf("Nirav Assar", "Lionel Messi")
-        val items = mapOf("team.name" to "evolution",
-                "team.color" to "green",
-                "team.player-names" to names)
+class TeamConfigurationTest {
+    //tag::teamConfigSpecNoBuilder[]
+    @Test
+    fun testTeamConfiguration() {
+        val names = Arrays.asList("Nirav Assar", "Lionel Messi")
+        val items: MutableMap<String, Any> = HashMap()
+        items["team.name"] = "evolution"
+        items["team.color"] = "green"
+        items["team.player-names"] = names
         val ctx = ApplicationContext.run(ApplicationContext::class.java, items) // <1>
-        `when`("TeamConfiguration is retrieved from the context") {
-            val teamConfiguration = ctx.getBean(TeamConfiguration::class.java)
-            then("configuration properties are populated") {
-                teamConfiguration.name shouldBe "evolution"
-                teamConfiguration.color shouldBe "green"
-                teamConfiguration.playerNames!!.size shouldBe names.size
-                names.forEach(Consumer { name: String? -> teamConfiguration.playerNames!!.contains(name) shouldBe true })
-            }
-        }
+        val teamConfiguration = ctx.getBean(TeamConfiguration::class.java)
+        Assertions.assertEquals("evolution", teamConfiguration.name)
+        Assertions.assertEquals("green", teamConfiguration.color)
+        Assertions.assertEquals(names.size, teamConfiguration.playerNames!!.size)
+        names.forEach(Consumer { name: String? -> Assertions.assertTrue(teamConfiguration.playerNames!!.contains(name!!)) })
         ctx.close()
     }
-})
+
+    //end::teamConfigSpecNoBuilder[]
+    @Test
+    fun testBuilderPatternPlainUsage() {
+        val teamAdmin = TeamAdmin.Builder().withManager("Nirav")
+                .withCoach("Tommy")
+                .withPresident("Mark").build()
+        Assertions.assertEquals("Nirav", teamAdmin.manager)
+        Assertions.assertEquals("Tommy", teamAdmin.coach)
+        Assertions.assertEquals("Mark", teamAdmin.president)
+    }
+
+    //tag::teamConfigSpecBuilder[]
+    @Test
+    fun testTeamConfigurationBuilder() {
+        val names = Arrays.asList("Nirav Assar", "Lionel Messi")
+        val items: MutableMap<String, Any> = HashMap()
+        items["team.name"] = "evolution"
+        items["team.color"] = "green"
+        items["team.team-admin.manager"] = "Jerry Jones" // <1>
+        items["team.team-admin.coach"] = "Tommy O'Neill"
+        items["team.team-admin.president"] = "Mark Scanell"
+        items["team.player-names"] = names
+        val ctx = ApplicationContext.run(ApplicationContext::class.java, items)
+        val teamConfiguration = ctx.getBean(TeamConfiguration::class.java)
+        val teamAdmin = teamConfiguration.builder.build() // <2>
+        Assertions.assertEquals("evolution", teamConfiguration.name)
+        Assertions.assertEquals("green", teamConfiguration.color)
+        Assertions.assertEquals("Nirav Assar", teamConfiguration.playerNames!![0])
+        Assertions.assertEquals("Lionel Messi", teamConfiguration.playerNames!![1])
+
+        // check the builder has values set
+        Assertions.assertEquals("Jerry Jones", teamConfiguration.builder.manager)
+        Assertions.assertEquals("Tommy O'Neill", teamConfiguration.builder.coach)
+        Assertions.assertEquals("Mark Scanell", teamConfiguration.builder.president)
+
+        // check the object can be built
+        Assertions.assertEquals("Jerry Jones", teamAdmin.manager) // <3>
+        Assertions.assertEquals("Tommy O'Neill", teamAdmin.coach)
+        Assertions.assertEquals("Mark Scanell", teamAdmin.president)
+        ctx.close()
+    } //end::teamConfigSpecBuilder[]
+}
